35005e5de19f94321b65e6d7b8afaf7c7aaf36b5
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-09-15 15:38:39 +0545

    center logo and color

--- grepship/components/navbar.js
+++ grepship/components/navbar.js
@@ -1,12 +1,9 @@
 export default function Navbar(){
 	return (
-		<nav className="flex fixed top-0 left-0 right-0 h-30 ">
-			<div className="items-center">
-				<a className="hover-scale-105">
-					<img
-						src="/logo.png"
-						alt="grepship"
-					/>
+		<nav className="flex fixed top-0 left-0 right-0 h-30  bg-[#26351F] color-[#FFF8E7]">
+			<div className="items-center h-full w-1/5 flex">
+				<a className="hover-scale-105 flex text-center w-full justify-center">
+					GrepShip
 				</a>
 			</div>
 			<div>
