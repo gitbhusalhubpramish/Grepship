60b8ec531c313c87f304b2d799a5ac6636777d4b
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-09-15 16:03:11 +0545

    search box icon

--- grepship/components/navbar.js
+++ grepship/components/navbar.js
@@ -6,8 +6,22 @@ export default function Navbar(){
 					GrepShip
 				</a>
 			</div>
-			<div>
-				<input className="" type="text"/>
+			<div className="flex items-center justify-center w-1/3">
+				<input className="pl-3 pr-10 bg-[#35452C] rounded-xl w-full h-1/3" placeholder="Harvest Friend" type="text"/>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					className="w-5 h-5 relative  right-7 h-full flex items-center justify-center text-gray-400"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M21 21l-4.35-4.35M17 11a6 6 0 11-12 0 6 6 0 0112 0z"
+					/>
+				</svg>
 			</div>
 		</nav>
 	)
