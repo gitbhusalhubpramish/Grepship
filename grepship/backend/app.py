fe58bde572afde7ef2b57171501f03c1ea13de46
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-09-15 13:25:08 +0545

    flask app init

---
+++ grepship/backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask-cors import CORS
+
+app = Flask(__name__)
+
+CORS(app, origins=["http://localhost:3000/])
+
+@app.get("/api/hello")
+def hello():
+	return {
+		"message": "hello"
+	}
+
+if __name__ == "__main__":
+	app.run(port=5000,debug=True)
