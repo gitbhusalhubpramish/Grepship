d051420589efb6072b3e11ca44c5c05535aa0f4f
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-09-15 13:55:10 +0545

    fixed bugs

--- grepship/backend/app.py
+++ grepship/backend/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask-cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, origins=["http://localhost:3000/])
+CORS(app, origins=["http://localhost:3000/"])
 
 @app.get("/api/hello")
 def hello():
