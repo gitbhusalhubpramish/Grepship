5ce445cb439e92705988c4352a800f892446af33
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-09-16 19:04:58 +0545

    ui done

--- grepship/app/signup/page.js
+++ grepship/app/signup/page.js
@@ -1,7 +1,7 @@
 export default function Signup(){
 	return (
 		<div className="bg-[#FFF8E7] h-screen w-screen pt-30 flex justify-center items-center">
-			<div className="text-[#6B8E3D] bg-[#FFFFFF] border-[#E4DFC9] border-2 min-w-15 min-h-85 p-5 w-1/5 h-2/3 rounded-3xl">
+			<div className="text-[#6B8E3D] bg-[#FFFFFF] border-[#E4DFC9] border-2 min-w-15 min-h-87 p-5 w-1/5 h-2/3 rounded-3xl">
 				<p className="font-bold text-3xl mb-5">Sign Up</p>
 				<label className=" w-full my-2">
 					Username: <br/>
@@ -16,7 +16,7 @@ export default function Signup(){
                                         <input name="password" type="password" className="border-[#a7e6a7] border-1 bg-[#e7ffe7]  rounded-lg mb-2 mt-1 w-full h-7" placeholder="Above Password"/>
                                 </label>
 				
-				<button className="w-full h-10 my-5">Join us</button>
+				<button className="bg-[#d9822b] text-[#FFFFFF] rounded-xl w-full h-10 my-5">Join us</button>
 			</div>
 		</div>
 	)
