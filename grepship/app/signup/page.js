59621ca10dc0fbc5ac149a7b94b63859fd901e02
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-09-16 18:03:14 +0545

    made card

---
+++ grepship/app/signup/page.js
@@ -0,0 +1,7 @@
+export default function Signup(){
+	return (
+		<div className="bg-[#FFF8E7] h-screen w-screen pt-30 flex justify-center items-center">
+			<div className="bg-[#FFFFFF] border-[#E4DFC9] border-2 min-w-15 min-h-70 w-1/5 h-2/3 rounded-3xl"></div>
+		</div>
+	)
+}
