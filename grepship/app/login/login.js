19143ee1d6ea008c1f5405ae01bc64107290e1d4
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-09-18 19:03:48 +0545

    fixed headers typo

--- grepship/app/login/login.js
+++ grepship/app/login/login.js
@@ -14,7 +14,7 @@ export default function Login(){
 	
 	const handelSubmit = async (e)=>{
 		e.preventDefault()
-		const res = await fetch("api/auth/login", {method: "POST", header:{"Content-Type":"application/json"}, body: JSON.stringify({form})})
+		const res = await fetch("api/auth/login", {method: "POST", headers:{"Content-Type":"application/json", "Accept": "application/json"}, body: JSON.stringify({username:form.username, password:form.password})})
 		if (res.status===200){
 			alert("Login successful")
 		}
