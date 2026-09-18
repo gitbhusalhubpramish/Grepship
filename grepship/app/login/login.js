3550936829403c378567ab8416c88298da3078c7
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-09-18 17:52:35 +0545

    card and login text

--- grepship/app/login/login.js
+++ grepship/app/login/login.js
@@ -4,6 +4,10 @@ import {useState, useEffect} from "react"
 
 export default function Login(){
 	return (
-		<div className="bg-[#FFF8E7] h-screen w-screen pt-30 flex justify-center items-center"></div>
+		<div className="bg-[#FFF8E7] h-screen w-screen pt-30 flex justify-center items-center">
+			<form className="text-[#6B8E3D] bg-[#ffffff] border-[#e4dfc9] border-2 min-w-15 min-h-87 p-5 w-1/5 h-2/3 rounded-3xl">
+				<p className="font-bond text-3xl mb-5">Login</p>
+			</form>
+		</div>
 	)
 }
